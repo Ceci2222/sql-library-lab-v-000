614c9ffcf75565b3dc2904f50a19b468786997d1
Marie Burns <mcburns2222@gmail.com>
2019-02-04 04:38:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,6 +43,6 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN character_books ON characters.id = character_books.character_id
   JOIN books ON character_books.book_id = books.id
   GROUP BY characters.id
-  ORDER BY COUNT(*) DESC;"
+  ORDER BY COUNT(*) DESC, characters.name;"
   
 end
